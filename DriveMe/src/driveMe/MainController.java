590d14ed58d4d4e5fb83fb3b5c630b12666dd532
sebs <sebs@server.batchcloud.local>
2020-02-18 14:43:02 +0100

    gb

--- DriveMe/src/driveMe/MainController.java
+++ DriveMe/src/driveMe/MainController.java
@@ -4,17 +4,27 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.Image;
 import java.awt.Insets;
 import java.awt.SystemColor;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
+import javax.swing.border.MatteBorder;
 
+import driveMe.vehicles.model.Vehicle;
 import driveMe.vehicles.service.VehiclesService;
+import net.miginfocom.swing.MigLayout;
 
 
 public class MainController {
@@ -25,6 +35,9 @@ public JFrame mainFrame=new JFrame("DriveMe");
 	private Color secondColor = new Color(238,238,238);
 	
 	
+	/**
+	 * @wbp.parser.entryPoint
+	 */
 	protected void initialize() {
 
 		//Setup Main frame
@@ -68,60 +81,198 @@ public JFrame mainFrame=new JFrame("DriveMe");
 		headerTopFlowLayout.setAlignment(FlowLayout.LEFT);
 		
 		//Add Buttons to header
-		JButton vehicleButton = new JButton("Fahrzeuge");
-		vehicleButton.setBorder(null);
-		vehicleButton.setBackground(secondColor);
-		vehicleButton.setMargin(new Insets(0, 0, 0, 0));
-		vehicleButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				mainFrame.getContentPane().removeAll();
-				mainFrame.getContentPane().add(vehicleContent());
-			}
-		});
-		vehicleButton.setPreferredSize(new Dimension(150, 35));
-		headerTop.add(vehicleButton);
-		
 		JButton customerButton = new JButton("Kunden");
 		customerButton.setForeground(Color.WHITE);
 		customerButton.setBorder(null);
-		customerButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				mainFrame.getContentPane().removeAll();
-	//						add customer page to contentPane
-				mainFrame.getContentPane().add(customerContent());
-			}
-		});
-		customerButton.setBackground(primaryColor);
+		customerButton.setBackground(secondColor);
 		customerButton.setMargin(new Insets(0, 0, 0, 0));
 		customerButton.setPreferredSize(new Dimension(150, 35));
+		
+		JButton vehicleButton = new JButton("Fahrzeuge");
+		vehicleButton.setBorder(null);
+		vehicleButton.setBackground(primaryColor);
+		vehicleButton.setMargin(new Insets(0, 0, 0, 0));
+		vehicleButton.setPreferredSize(new Dimension(150, 35));
+		headerTop.add(vehicleButton);
+		
+		customerButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				SwingUtilities.updateComponentTreeUI(mainFrame.getContentPane().add(customerContent(customerButton, vehicleButton)));
+			}
+		});
+		vehicleButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				SwingUtilities.updateComponentTreeUI(mainFrame.getContentPane().add(vehicleContent(customerButton, vehicleButton)));
+			}
+		});
 		headerTop.add(customerButton);
 	}
 	
 	private void setUpHeaderBottom(JPanel headerPanel) 
 	{
-		//TODO
+		//Set header bottom
+		JPanel headerBottom = new JPanel();
+		headerPanel.add(headerBottom, BorderLayout.CENTER);
+		headerBottom.setPreferredSize(new Dimension(10, 20));
+		headerBottom.setMinimumSize(new Dimension(10, 27));
+		headerBottom.setBackground(secondColor);
+		headerBottom.setLayout(new BorderLayout(0, 0));
+		
+		//Set leftSidePanel to header bottom
+		JPanel leftSidePanel = new JPanel();
+		leftSidePanel.setPreferredSize(new Dimension(200, 10));
+		headerBottom.add(leftSidePanel, BorderLayout.WEST);
+
+		//Set rightSidePanel to header bottom
+		JPanel rightSidePanel = new JPanel();
+		rightSidePanel.setPreferredSize(new Dimension(200, 10));
+		rightSidePanel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		headerBottom.add(rightSidePanel, BorderLayout.EAST);
+		//Add compobox to rightSidePanel
+		addComboBoxToPanel(rightSidePanel);
+		
+		//Set topSidePanel to header bottom
+		JPanel topSidePanel = new JPanel();
+		headerBottom.add(topSidePanel, BorderLayout.NORTH);
+
+		//Set bottomSidePanel to header bottom		
+		JPanel bottomSidePanel = new JPanel();
+		headerBottom.add(bottomSidePanel, BorderLayout.SOUTH);
+		
+		//Set midSidePanel to header bottom
+		JPanel midSidePanel = new JPanel();
+		midSidePanel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		headerBottom.add(midSidePanel, BorderLayout.CENTER);
+		//Set textField to midSidePanel
+		JTextField textField = new JTextField();
+		textField.setPreferredSize(new Dimension(400, 25));
+		textField.setSize(new Dimension(400, 24));
+		midSidePanel.add(textField);
+		textField.setColumns(50);
 	}
 
-	private JPanel vehicleContent() 
+	private JPanel vehicleContent(JButton customerButton, JButton vehicleButton) 
 	{
 		JPanel vehiclePanel = new JPanel();
 		vehiclePanel.setPreferredSize(new Dimension(10, 90));
 		vehiclePanel.setMinimumSize(new Dimension(10, 128));
-		vehiclePanel.setBackground(SystemColor.windowBorder);
+		vehiclePanel.setBackground(SystemColor.GREEN);
 		vehiclePanel.setLayout(new BorderLayout(0, 0));
+		vehiclePanel.setLayout(new MigLayout("", "[320px]", "[100px]"));
+		
+		customerButton.setBackground(primaryColor);
+		vehicleButton.setBackground(secondColor);
+		
+		ArrayList<Vehicle> vehicles = VehiclesService.findVehiclesByAll();
+		int i = 0;
+		for (Vehicle currentVehicle : vehicles)
+		{
+			String position = "cell 0 "+ i + ",grow";
+			vehiclePanel.add(createVehiclePanel(currentVehicle), position );
+			i++;
+		}
 		
 		return vehiclePanel;
 	} 
 	
-	private JPanel customerContent() 
+	private JPanel customerContent(JButton customerButton, JButton vehicleButton) 
 	{
 		JPanel customerPanel = new JPanel();
 		customerPanel.setPreferredSize(new Dimension(10, 90));
 		customerPanel.setMinimumSize(new Dimension(10, 128));
-		customerPanel.setBackground(SystemColor.windowBorder);
+		customerPanel.setBackground(SystemColor.RED);
 		customerPanel.setLayout(new BorderLayout(0, 0));
 		
+		customerButton.setBackground(secondColor);
+		vehicleButton.setBackground(primaryColor);
 		
 		return customerPanel;
+	}
+	
+	private void addComboBoxToPanel(JPanel panel)
+	{
+		JComboBox<String> comboBox = new JComboBox<String>();
+		
+		comboBox.setForeground(Color.WHITE);
+		comboBox.setBorder(null);
+		comboBox.setBackground(primaryColor);
+		comboBox.setPreferredSize(new Dimension(145, 25));
+		comboBox.addItem("Filtern");
+		comboBox.addItem("test");
+		
+		panel.add(comboBox);
+	}
+	
+	private JPanel createVehiclePanel(Vehicle currentVehicle) 
+	{
+		JPanel vehiclePanel = new JPanel();
+		vehiclePanel.setLayout(new BorderLayout(0, 0));
+		
+		JPanel innerVehiclePanel = new JPanel();
+		innerVehiclePanel.setBorder(new MatteBorder(0, 0, 1, 0, (Color) new Color(0, 0, 0)));
+		innerVehiclePanel.setPreferredSize(new Dimension(10, 50));
+		innerVehiclePanel.setLayout(null);
+			
+		JLabel vehicleDetails = new JLabel("[34343434] Mercedes Benz A-Klasse");
+		vehicleDetails.setBounds(59, 11, 200, 14);
+		innerVehiclePanel.add(vehicleDetails);
+	
+		JLabel vehicleLocation = new JLabel("Beinberger Weg 11, 75394 Oberreichenbach");
+		vehicleLocation.setFont(new Font("Tahoma", Font.PLAIN, 9));
+		vehicleLocation.setBounds(59, 25, 261, 14);
+		innerVehiclePanel.add(vehicleLocation);
+	
+		JButton moreDetailsBtn = new JButton("");
+//		moreDetailsBtn.setIcon(resizeImageIcon1(new ImageIcon(DriveMe.class.getResource("/test121/standort.png")), 15, 15));
+		moreDetailsBtn.setBorder(null);
+		moreDetailsBtn.setPreferredSize(new Dimension(10, 10));
+		moreDetailsBtn.setBounds(294, 11, 26, 23);
+		innerVehiclePanel.add(moreDetailsBtn);
+		
+		JButton reservationBtn = new JButton("");
+//		reservationBtn.setIcon(resizeImageIcon1(new ImageIcon(DriveMe.class.getResource("/test121/menu.png")), 15, 15));
+		reservationBtn.setBorder(null);
+		reservationBtn.setPreferredSize(new Dimension(10, 10));
+		reservationBtn.setBounds(269, 11, 26, 23);
+		innerVehiclePanel.add(reservationBtn);
+				
+		vehiclePanel.add(innerVehiclePanel, BorderLayout.NORTH);
+
+		JPanel carDetails = new JPanel();
+		carDetails.setLayout(null);
+		
+		if(currentVehicle.isReserved())
+		{
+			JLabel lblReserved = new JLabel("Reserviert von: " + currentVehicle.getReservedFrom() + " bis: " + currentVehicle.getReservedTo());
+			lblReserved.setFont(new Font("Tahoma", Font.PLAIN, 9));
+			lblReserved.setBounds(25, 25, 103, 14);
+			carDetails.add(lblReserved);
+			
+			JLabel lblLocation = new JLabel("Aktueller Standort: " + currentVehicle.getLatitude() + " " + currentVehicle.getLongitude());
+			lblLocation.setBounds(25, 11, 48, 14);
+			carDetails.add(lblLocation);
+		}
+		else
+		{
+			JLabel lblReserved = new JLabel("Keine Buchung");
+			lblReserved.setFont(new Font("Tahoma", Font.PLAIN, 9));
+			lblReserved.setBounds(25, 25, 103, 14);
+			carDetails.add(lblReserved);
+			
+			JLabel lblLocation = new JLabel("Aktueller Standort: ");
+			lblLocation.setBounds(25, 11, 48, 14);
+			carDetails.add(lblLocation);
+		}
+				
+		vehiclePanel.add(carDetails, BorderLayout.CENTER);
+			
+		return vehiclePanel;
+	}
+	
+	private ImageIcon resizeImageIcon1(ImageIcon imageIcon, int heigth, int width) 
+	{
+		Image image = imageIcon.getImage(); // transform it 
+		Image newimg = image.getScaledInstance(heigth, width,  java.awt.Image.SCALE_SMOOTH); // scale it the smooth way  
+		return imageIcon = new ImageIcon(newimg);
 	}
 }
--- DriveMe/src/driveMe/vehicles/model/Vehicle.java
+++ DriveMe/src/driveMe/vehicles/model/Vehicle.java
@@ -1,5 +1,6 @@
 package driveMe.vehicles.model;
 
+import java.sql.Date;
 
 public class Vehicle {
 
@@ -12,9 +13,13 @@ public class Vehicle {
 	String 	latitude;
 	String image;
 	String fuel;
+	Date reservedFrom;
+	Date reservedTo;
+	boolean isReserved;
 	
-	
-	public Vehicle (int id, String model, String brand, int ps, int seats, String longitude, String latitude, String image, String fuel){
+	public Vehicle (int id, String model, String brand, int ps, int seats, String longitude, 
+					String latitude, String image, String fuel,Date reservedFrom, Date reservedTo, boolean isReserved)
+	{
 		this.id = id;
 		this.model = model;
 		this.brand = brand;
@@ -24,94 +29,92 @@ public class Vehicle {
 		this.latitude = latitude;
 		this.image = image;
 		this.fuel = fuel;
+		this.reservedFrom = reservedFrom;
+		this.reservedTo = reservedTo;
+		this.isReserved = isReserved;
 	}
 
+
+	public Date getReservedFrom() {
+		return reservedFrom;
+	}
+	public void setReservedFrom(Date reservedFrom) {
+		this.reservedFrom = reservedFrom;
+	}
+
+	public Date getReservedTo() {
+		return reservedTo;
+	}
+	public void setReservedTo(Date reservedTo) {
+		this.reservedTo = reservedTo;
+	}
+
+	public boolean isReserved() {
+		return isReserved;
+	}
+	public void setReserved(boolean isReserved) {
+		this.isReserved = isReserved;
+	}
 
 	public int getId() {
 		return id;
 	}
-
-
 	public void setId(int id) {
 		this.id = id;
 	}
 
-
 	public String getModel() {
 		return model;
 	}
-
-
 	public void setModel(String model) {
 		this.model = model;
 	}
 
-
 	public String getBrand() {
 		return brand;
 	}
-
-
 	public void setBrand(String brand) {
 		this.brand = brand;
 	}
 
-
 	public int getPs() {
 		return ps;
 	}
-
-
 	public void setPs(int ps) {
 		this.ps = ps;
 	}
 
-
 	public int getSeats() {
 		return seats;
 	}
-
-
 	public void setSeats(int seats) {
 		this.seats = seats;
 	}
 
-
 	public String getLongitude() {
 		return longitude;
 	}
-
-
 	public void setLongitude(String longitude) {
 		this.longitude = longitude;
 	}
 
-
 	public String getLatitude() {
 		return latitude;
 	}
-
-
 	public void setLatitude(String latitude) {
 		this.latitude = latitude;
 	}
 
-
 	public String getImage() {
 		return image;
 	}
-
-
 	public void setImage(String image) {
 		this.image = image;
 	}
 
-
 	public String getFuel() {
 		return fuel;
 	}
-
-
 	public void setFuel(String fuel) {
 		this.fuel = fuel;
 	}
