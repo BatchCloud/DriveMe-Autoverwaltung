21f271f6ea78112ba359e249dd4c31f418d67b1e
tfauss <tfauss@server.batchcloud.local>
2020-02-18 09:58:55 +0100

    RunApplication and MainController

--- DriveMe/src/driveMe/MainController.java
+++ DriveMe/src/driveMe/MainController.java
@@ -1,5 +1,87 @@
 package driveMe;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Insets;
+import java.awt.SystemColor;
+import java.awt.Window.Type;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+
 public class MainController {
 
+	public JFrame mainFrame = new JFrame("DriveMe");
+	
+	private Color primaryColor = new Color(105,157,217);
+	private Color secondColor = new Color(238,238,238);
+	
+	/**
+	 * @wbp.parser.entryPoint
+	 */
+	protected void initialize() {
+
+		//Setup Main frame
+		mainFrame.setType(Type.POPUP);
+		mainFrame.setBackground(primaryColor);
+		mainFrame.getContentPane().setMinimumSize(new Dimension(0, 16));
+		mainFrame.setBounds(100, 100, 1106, 661);
+		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		mainFrame.getContentPane().setLayout(new BorderLayout(0, 0));
+		
+		//Setup header pannel
+		JPanel headerPanel = new JPanel();
+		headerPanel.setPreferredSize(new Dimension(10, 90));
+		headerPanel.setMinimumSize(new Dimension(10, 128));
+		headerPanel.setBackground(SystemColor.windowBorder);
+		mainFrame.getContentPane().add(headerPanel, BorderLayout.NORTH);
+		headerPanel.setLayout(new BorderLayout(0, 0));
+		
+		//Setup header top to header pannel
+		JPanel headerTop = new JPanel();
+		headerPanel.add(headerTop, BorderLayout.NORTH);
+		headerTop.setMaximumSize(new Dimension(32767, 0));
+		headerTop.setMinimumSize(new Dimension(0, 0));
+		headerTop.setPreferredSize(new Dimension(0, 35));
+		headerTop.setBackground(primaryColor);
+		
+		///
+			/// FlowLayout?
+		///
+		
+		//Add Buttons to header
+		JButton vehicleButton = new JButton("Fahrzeuge");
+		vehicleButton.setBorder(null);
+		vehicleButton.setBackground(secondColor);
+		vehicleButton.setMargin(new Insets(0, 0, 0, 0));
+		vehicleButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+		vehicleButton.setPreferredSize(new Dimension(150, 35));
+		headerTop.add(vehicleButton);
+		
+		JButton customerButton = new JButton("Kunden");
+		customerButton.setForeground(Color.WHITE);
+		customerButton.setBorder(null);
+		customerButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				mainFrame.getContentPane().removeAll();
+//				add customer page to contentPane
+//				mainFrame.getContentPane().add());
+			}
+		});
+		customerButton.setBackground(primaryColor);
+		customerButton.setMargin(new Insets(0, 0, 0, 0));
+		customerButton.setPreferredSize(new Dimension(150, 35));
+		headerTop.add(customerButton);
+		
+	}
+	
 }
---
+++ DriveMe/src/driveMe/RunApplication.java
@@ -0,0 +1,38 @@
+package driveMe;
+
+import java.awt.Color;
+import java.awt.EventQueue;
+
+import javax.swing.UIManager;
+import javax.swing.plaf.ColorUIResource;
+
+public class RunApplication extends MainController{
+
+	public static void main(String[] args) {
+		
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+					 UIManager.put("ComboBox.background", new ColorUIResource(Color.white));
+					 UIManager.put("JTextField.background", new ColorUIResource(Color.white));
+				        UIManager.put("ComboBox.selectionBackground", new ColorUIResource(Color.lightGray));
+				        UIManager.put("ComboBox.selectionForeground", new ColorUIResource(Color.black));
+				        UIManager.put("TabbedPane.borderColor", Color.RED);
+				        UIManager.put("TabbedPane.tabAreaBackground", Color.RED);
+				        UIManager.put("TabbedPane.unselectedTabBackground", Color.RED);
+					MainController window = new MainController();
+					window.mainFrame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+	
+	public RunApplication()
+	{
+		initialize();
+	}
+	
+}
